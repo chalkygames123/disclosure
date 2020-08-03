297c1b8b0e4bebac6e19d60268541d38e364bfab
Takuya Fukuju <chalkygames123@gmail.com>
2020-08-03 16:35:32 +0900

    ESLint/stylelint/Prettier の設定を更新

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   semi: false,
   singleQuote: true,
+  overrides: [
+    {
+      files: '*.html',
+      options: {
+        printWidth: Infinity,
+      },
+    },
+  ],
 }
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
   extends: [
-    'stylelint-config-twbs-bootstrap/scss',
+    'stylelint-config-twbs-bootstrap/css',
     'stylelint-prettier/recommended',
   ],
   rules: {
+    'alpha-value-notation': 'number',
     'at-rule-empty-line-before': [
       'always',
       {
@@ -13,40 +14,25 @@ module.exports = {
       },
     ],
     'declaration-block-no-redundant-longhand-properties': true,
+    'declaration-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested', 'after-declaration'],
+        ignore: ['after-comment'],
+      },
+    ],
     'function-calc-no-invalid': true,
+    'hue-degree-notation': 'angle',
     'no-empty-source': null,
     'order/order': [
       [
         'custom-properties',
-        'dollar-variables',
-        {
-          type: 'at-rule',
-          name: 'extend',
-        },
-        {
-          type: 'at-rule',
-          name: 'include',
-          hasBlock: false,
-        },
         'declarations',
         {
           type: 'at-rule',
           hasBlock: true,
         },
-        {
-          type: 'at-rule',
-          name: 'include',
-          hasBlock: true,
-        },
         'rules',
-        {
-          type: 'rule',
-          selector: /^&::\w/,
-        },
-        {
-          type: 'rule',
-          selector: /^&:\w/,
-        },
       ],
     ],
     'rule-empty-line-before': [
@@ -56,37 +42,6 @@ module.exports = {
         ignore: ['after-comment'],
       },
     ],
-    'scss/at-each-key-value-single-line': true,
-    'scss/at-if-no-null': true,
-    'scss/at-import-partial-extension': 'never',
-    'scss/at-rule-conditional-no-parentheses': true,
-    'scss/comment-no-loud': true,
-    'scss/declaration-nested-properties': 'never',
-    'scss/dimension-no-non-numeric-values': true,
-    'scss/dollar-variable-empty-line-before': [
-      'always',
-      {
-        except: ['first-nested', 'after-dollar-variable'],
-        ignore: ['after-comment'],
-      },
-    ],
-    'scss/double-slash-comment-inline': 'never',
-    'scss/double-slash-comment-whitespace-inside': 'always',
-    'scss/function-color-relative': true,
-    'scss/function-quote-no-quoted-strings-inside': true,
-    'scss/function-unquote-no-unquoted-strings-inside': true,
-    'scss/no-duplicate-dollar-variables': [
-      true,
-      {
-        ignoreInsideAtRules: ['if', 'mixin'],
-      },
-    ],
-    'scss/operator-no-newline-after': null,
-    'scss/operator-no-newline-before': null,
-    'scss/operator-no-unspaced': null,
-    'scss/partial-no-import': true,
-    'scss/selector-no-redundant-nesting-selector': true,
-    'scss/selector-no-union-class-name': true,
     'selector-class-pattern': null,
   },
 }
