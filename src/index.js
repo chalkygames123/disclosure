8dd134c9cea5f7cf1fe34ec02bc5091ec7ba1fbb
Takuya Fukuju <chalkygames123@gmail.com>
2021-08-21 18:44:06 +0900

    フォーマットを実行

--- src/index.js
+++ src/index.js
@@ -3,33 +3,33 @@ import { Disclosure } from './modules'
 const disclosures = []
 
 for (const el of document.querySelectorAll('[data-disclosure]')) {
-  const disclosure = new Disclosure(el, {
-    hashNavigation: true,
-  })
+	const disclosure = new Disclosure(el, {
+		hashNavigation: true,
+	})
 
-  disclosures.push(disclosure)
+	disclosures.push(disclosure)
 }
 
 document.querySelector('#open-all').addEventListener('click', () => {
-  for (const el of disclosures) {
-    el.open()
-  }
+	for (const el of disclosures) {
+		el.open()
+	}
 })
 
 document.querySelector('#close-all').addEventListener('click', () => {
-  for (const el of disclosures) {
-    el.close()
-  }
+	for (const el of disclosures) {
+		el.close()
+	}
 })
 
 const summary04El = document.querySelector(
-  '[data-disclosure][aria-controls="disclosure-04-details"]'
+	'[data-disclosure][aria-controls="disclosure-04-details"]'
 )
 const eventTypes = ['open', 'opened', 'close', 'closed']
 const eventTypeEl = document.querySelector('#event-type')
 
 for (const type of eventTypes) {
-  summary04El.addEventListener(type, (e) => {
-    eventTypeEl.textContent = e.type
-  })
+	summary04El.addEventListener(type, (e) => {
+		eventTypeEl.textContent = e.type
+	})
 }
--- src/modules/disclosure.js
+++ src/modules/disclosure.js
@@ -26,168 +26,168 @@
  * ```
  */
 export default class {
-  /**
-   * @param {HTMLElement} summaryEl - 概要要素
-   * @param {Object} options - オプション
-   * @param {boolean} options.hashNavigation - 初期化時、URL フラグメントに一致する ID を持つ概要要素に対応する詳細要素を開くかどうか
-   */
-  constructor(summaryEl, options) {
-    this.summaryEl = summaryEl
-    this.options = {
-      hashNavigation: false,
-      ...options,
-    }
+	/**
+	 * @param {HTMLElement} summaryEl - 概要要素
+	 * @param {Object} options - オプション
+	 * @param {boolean} options.hashNavigation - 初期化時、URL フラグメントに一致する ID を持つ概要要素に対応する詳細要素を開くかどうか
+	 */
+	constructor(summaryEl, options) {
+		this.summaryEl = summaryEl
+		this.options = {
+			hashNavigation: false,
+			...options,
+		}
 
-    this.init()
-  }
+		this.init()
+	}
 
-  get noTransition() {
-    const detailsElComputedStyle = getComputedStyle(this.detailsEl)
+	get noTransition() {
+		const detailsElComputedStyle = getComputedStyle(this.detailsEl)
 
-    return (
-      detailsElComputedStyle.transitionDuration === '0s' &&
-      detailsElComputedStyle.transitionDelay === '0s'
-    )
-  }
+		return (
+			detailsElComputedStyle.transitionDuration === '0s' &&
+			detailsElComputedStyle.transitionDelay === '0s'
+		)
+	}
 
-  init() {
-    this.handleSummaryElClick = this.handleSummaryElClick.bind(this)
-    this.summaryEl.addEventListener('click', this.handleSummaryElClick)
+	init() {
+		this.handleSummaryElClick = this.handleSummaryElClick.bind(this)
+		this.summaryEl.addEventListener('click', this.handleSummaryElClick)
 
-    this.detailsEl = document.querySelector(
-      `#${this.summaryEl.getAttribute('aria-controls')}`
-    )
+		this.detailsEl = document.querySelector(
+			`#${this.summaryEl.getAttribute('aria-controls')}`
+		)
 
-    this.closeEls = this.detailsEl.querySelectorAll('[data-disclosure-close]')
-    this.handleCloseElClick = this.handleCloseElClick.bind(this)
+		this.closeEls = this.detailsEl.querySelectorAll('[data-disclosure-close]')
+		this.handleCloseElClick = this.handleCloseElClick.bind(this)
 
-    for (const el of this.closeEls) {
-      el.addEventListener('click', this.handleCloseElClick)
-    }
+		for (const el of this.closeEls) {
+			el.addEventListener('click', this.handleCloseElClick)
+		}
 
-    this.handleTransitionEnd = this.handleTransitionEnd.bind(this)
+		this.handleTransitionEnd = this.handleTransitionEnd.bind(this)
 
-    if (
-      this.detailsEl.getAttribute('aria-hidden') !== 'true' ||
-      (this.options.hashNavigation &&
-        this.summaryEl.id &&
-        this.summaryEl.id === window.location.hash.slice(1))
-    ) {
-      if (this.noTransition) {
-        this.open()
-      } else {
-        this.detailsEl.style.transition = 'none'
-        this.open()
-        this.detailsEl.style.transition = ''
-      }
-    }
-  }
+		if (
+			this.detailsEl.getAttribute('aria-hidden') !== 'true' ||
+			(this.options.hashNavigation &&
+				this.summaryEl.id &&
+				this.summaryEl.id === window.location.hash.slice(1))
+		) {
+			if (this.noTransition) {
+				this.open()
+			} else {
+				this.detailsEl.style.transition = 'none'
+				this.open()
+				this.detailsEl.style.transition = ''
+			}
+		}
+	}
 
-  scrollIntoViewIfNeeded() {
-    const summaryElClientRect = this.summaryEl.getBoundingClientRect()
+	scrollIntoViewIfNeeded() {
+		const summaryElClientRect = this.summaryEl.getBoundingClientRect()
 
-    if (
-      summaryElClientRect.top < 0 ||
-      window.innerHeight < summaryElClientRect.bottom
-    ) {
-      this.summaryEl.scrollIntoView({
-        block: 'center',
-      })
-    }
-  }
+		if (
+			summaryElClientRect.top < 0 ||
+			window.innerHeight < summaryElClientRect.bottom
+		) {
+			this.summaryEl.scrollIntoView({
+				block: 'center',
+			})
+		}
+	}
 
-  open() {
-    if (this.isOpen) return
+	open() {
+		if (this.isOpen) return
 
-    this.isOpen = true
+		this.isOpen = true
 
-    this.detailsEl.style.height = `${this.detailsEl.scrollHeight}px`
+		this.detailsEl.style.height = `${this.detailsEl.scrollHeight}px`
 
-    this.detailsEl.removeAttribute('aria-hidden')
-    this.summaryEl.setAttribute('aria-expanded', 'true')
+		this.detailsEl.removeAttribute('aria-hidden')
+		this.summaryEl.setAttribute('aria-expanded', 'true')
 
-    if (this.noTransition) {
-      this.cleanUp()
-    } else {
-      this.detailsEl.addEventListener('transitionend', this.handleTransitionEnd)
-    }
+		if (this.noTransition) {
+			this.cleanUp()
+		} else {
+			this.detailsEl.addEventListener('transitionend', this.handleTransitionEnd)
+		}
 
-    this.emit('open')
-  }
+		this.emit('open')
+	}
 
-  close() {
-    if (!this.isOpen) return
+	close() {
+		if (!this.isOpen) return
 
-    this.isOpen = false
+		this.isOpen = false
 
-    if (!this.noTransition) {
-      this.detailsEl.style.height = `${this.detailsEl.scrollHeight}px`
-      // eslint-disable-next-line no-unused-expressions
-      this.detailsEl.clientHeight // レイアウトを強制する (参考: https://gist.github.com/paulirish/5d52fb081b3570c81e3a)
-    }
+		if (!this.noTransition) {
+			this.detailsEl.style.height = `${this.detailsEl.scrollHeight}px`
+			// eslint-disable-next-line no-unused-expressions
+			this.detailsEl.clientHeight // レイアウトを強制する (参考: https://gist.github.com/paulirish/5d52fb081b3570c81e3a)
+		}
 
-    this.detailsEl.style.height = '0'
+		this.detailsEl.style.height = '0'
 
-    this.detailsEl.setAttribute('aria-hidden', 'true')
-    this.summaryEl.setAttribute('aria-expanded', 'false')
+		this.detailsEl.setAttribute('aria-hidden', 'true')
+		this.summaryEl.setAttribute('aria-expanded', 'false')
 
-    if (this.noTransition) {
-      this.cleanUp()
-    } else {
-      this.detailsEl.addEventListener('transitionend', this.handleTransitionEnd)
-    }
+		if (this.noTransition) {
+			this.cleanUp()
+		} else {
+			this.detailsEl.addEventListener('transitionend', this.handleTransitionEnd)
+		}
 
-    this.emit('close')
-  }
+		this.emit('close')
+	}
 
-  toggle() {
-    if (this.isOpen) {
-      this.close()
-    } else {
-      this.open()
-    }
-  }
+	toggle() {
+		if (this.isOpen) {
+			this.close()
+		} else {
+			this.open()
+		}
+	}
 
-  cleanUp() {
-    if (!this.noTransition) {
-      this.detailsEl.removeEventListener(
-        'transitionend',
-        this.handleTransitionEnd
-      )
-    }
+	cleanUp() {
+		if (!this.noTransition) {
+			this.detailsEl.removeEventListener(
+				'transitionend',
+				this.handleTransitionEnd
+			)
+		}
 
-    this.detailsEl.style.height = ''
+		this.detailsEl.style.height = ''
 
-    if (this.isOpen) {
-      this.emit('opened')
-    } else {
-      this.emit('closed')
-    }
-  }
+		if (this.isOpen) {
+			this.emit('opened')
+		} else {
+			this.emit('closed')
+		}
+	}
 
-  emit(type) {
-    this.summaryEl.dispatchEvent(
-      new CustomEvent(type, {
-        bubbles: true,
-      })
-    )
-  }
+	emit(type) {
+		this.summaryEl.dispatchEvent(
+			new CustomEvent(type, {
+				bubbles: true,
+			})
+		)
+	}
 
-  handleSummaryElClick() {
-    this.toggle()
-  }
+	handleSummaryElClick() {
+		this.toggle()
+	}
 
-  handleCloseElClick() {
-    this.close()
+	handleCloseElClick() {
+		this.close()
 
-    this.summaryEl.focus()
+		this.summaryEl.focus()
 
-    this.scrollIntoViewIfNeeded()
-  }
+		this.scrollIntoViewIfNeeded()
+	}
 
-  handleTransitionEnd(e) {
-    if (e.target !== this.detailsEl) return
+	handleTransitionEnd(e) {
+		if (e.target !== this.detailsEl) return
 
-    this.cleanUp()
-  }
+		this.cleanUp()
+	}
 }
