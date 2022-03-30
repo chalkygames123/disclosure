312fcbe4dd99faf8c76675ef5a4d3920252baa0f
Takuya Fukuju <chalkygames123@gmail.com>
2022-03-31 03:40:32 +0900

    tmp

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,31 @@
+// これは `overrides` 内の `extends` がトップレベルのルールを上書きしてしまうため必要
+const baseRules = {
+	'capitalized-comments': 'off',
+	'class-methods-use-this': 'error',
+	'unicorn/prevent-abbreviations': 'off',
+};
+
 module.exports = {
 	root: true,
 	extends: ['xo', 'plugin:unicorn/recommended', 'prettier'],
 	rules: {
-		'class-methods-use-this': 'error',
-		'unicorn/prevent-abbreviations': 'off',
+		...baseRules,
 	},
 	overrides: [
 		{
 			files: '**/*.cjs',
+			parserOptions: {
+				sourceType: 'script',
+			},
 		},
 		{
 			files: 'src/**',
-			extends: ['xo/browser', 'prettier'],
+			extends: ['xo/browser', 'plugin:unicorn/recommended', 'prettier'],
 			parser: '@babel/eslint-parser',
 			plugins: ['@babel'],
+			rules: {
+				...baseRules,
+			},
 		},
 	],
 };
